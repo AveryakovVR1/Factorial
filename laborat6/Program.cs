c531b4bec94c48649b0d0fb4589c3c62304007f3
AveryakovVR <AveryakovVR@310D-7>
2022-10-08 11:40:09 +0700

    Добавьте файлы проекта.

---
+++ laborat6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+
+namespace laborat6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число для подсчёта его факториала:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int x = factorial(y);
+            Console.WriteLine($"результат: {x}");
+        }
+        static BigInteger factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * factorial(n - 1);
+            }
+        }
+       
+    }
+}
